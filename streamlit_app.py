28a6814e120c950a0adbf5b2c3aea53321d84853
Arjun <66225751+ArjSan1@users.noreply.github.com>
2023-03-05 09:49:21 -0600

    Finished Artifier

---
+++ streamlit_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+#st.title('The Artifier')
+
+global image_size
+#image_size = st.selectbox('Select an image size', ['256x256', '512x512', '1024x1024'])
+
+global art_style
+#art_style = st.selectbox('Select an art style', ['Watercolor', 'Sketch', 'Pop Art'])
+
+
+
+global text_input
+#user_input = st.text_input("Enter the url of an image", "Enter a url")
+
